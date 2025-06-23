fa2dcf6d1b2a8d7b7e4a171ff4f59574fca3a278
MohamedAhmedDev <mohamedahmeddev333@gmail.com>
2025-06-23 19:36:09 +0300

    small change on users table, and solve small conflict

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,6 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->rememberToken();
             $table->string('phone', 20)->unique();
             $table->boolean('is_verified')->default(false);
             $table->timestamps();
