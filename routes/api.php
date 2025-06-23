d29aabc1b612be0a73dc56069235a2c8436a07a7
MohamedAhmedDev <mohamedahmeddev333@gmail.com>
2025-06-23 17:10:28 +0300

    Add Notification API (index, mark as read, mark all as read, delete)

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\UserAuthController;
 use App\Http\Controllers\UserProfileController;
+use App\Http\Controllers\NotificationController;
+use App\Http\Controllers\NotificationManagementController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,4 +11,15 @@ use Illuminate\Support\Facades\Route;
 Route::controller(UserAuthController::class)->prefix("auth")->group(function () {
     Route::post("send-otp", "sendOtp");
     Route::post("verify-otp", "verifyOtp");
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    // Notification Routes
+    Route::controller(NotificationController::class)->prefix("notifications")->group(function () {
+        Route::get("/", "index");                                  
+        Route::patch("/{notificationId}/read", "markAsRead");
+        Route::patch("/mark-all-read", "markAllAsRead");
+        Route::delete("/{notificationId}", "destroy");
+    });
+
 });
