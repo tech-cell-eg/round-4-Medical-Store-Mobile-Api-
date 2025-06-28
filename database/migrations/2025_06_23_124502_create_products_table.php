454c89e4e27c1f0d28b34e2224aaadac9fac2fb0
abanoub-talaat <105149048+abanoub-talaat@users.noreply.github.com>
2025-06-28 10:50:59 +0300

    WIP

--- database/migrations/2025_06_23_124502_create_products_table.php
+++ database/migrations/2025_06_23_124502_create_products_table.php
@@ -11,6 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
+        // drop the table if it exists
+        Schema::dropIfExists('products');
+
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
